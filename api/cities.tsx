a8d7eddfb2d8bb20822aa5df2898b778d059ef23
Theodor Khademi <theo.khademi@gmail.com>
2022-01-30 12:38:12 +0100

    configured project to handle environment variables

---
+++ api/cities.tsx
@@ -0,0 +1,11 @@
+/**
+ * API for fetching information about cities 
+ */
+
+import { API_PATH } from "@env"
+
+
+export const fetchCityPopulation = (cityInput:string) => {
+
+    const apiPath = process.env.API_PATH
+}
