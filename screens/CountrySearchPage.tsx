062d18ca83426408b887402f3d0858ca72cfb488
Theodor Khademi <theo.khademi@gmail.com>
2022-01-31 18:09:29 +0100

    add states of page

--- screens/CountrySearchPage.tsx
+++ screens/CountrySearchPage.tsx
@@ -9,9 +9,9 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack'
 import { NavigatorParamsList } from '../types'
 
+import React, { useState } from 'react';
+
 import { fetchThreeMostPopulatedCities } from '../api/Countries';
-
-
 
 import { PageText } from '../components/PageText'
 import { NavigationButton } from '../components/NavigationButton' // will be used later after search
@@ -24,6 +24,14 @@ interface Props{
 
 // TODO: add functionality 
 export const CountrySearchPage = ( props:Props ) => {
+
+    const [countryInput, setCountryInput] = useState('')
+    const [succesfulSearch, setSuccesfulSearch] = useState(false)
+    const [result, setResult] : [string[], Function]  = useState([])
+    const [displayErrorMessage, setDisplayErrorMessage] = useState(false)
+
+
+
     return (
         <View style={{marginTop:200}}> 
             <BackButton navigation={props.navigation}/>
