aed8024dcb8e26e72020fc48df4f8bdf42161908
Theodor Khademi <theo.khademi@gmail.com>
2022-02-02 13:32:35 +0100

    updated main cointainer

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -3,6 +3,8 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     mainContainer: {
         display: 'flex',
-        flexDirection: 'column',         
+        flex: 1,
+        flexDirection: 'column', 
+        backgroundColor: "#eaeaea"        
     }
 }) 
