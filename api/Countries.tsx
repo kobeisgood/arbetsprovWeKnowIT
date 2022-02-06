893319c421277cd4800478d248b41259b8ba00d3
Theodor Khademi <theo.khademi@gmail.com>
2022-02-06 14:53:20 +0100

    Updated comment and code structure for api

--- api/Countries.tsx
+++ api/Countries.tsx
@@ -1,5 +1,5 @@
 /**
- * API for fetching information about countries
+ * Methods for fetching information about countries
  */
 
 
@@ -8,18 +8,16 @@
  * Needs to "convert" country name into a country code in the api first to find correct names and cities 
  * 
  * @param countryInput The country the user has inputted
- * @returns List of top cities 
+ * @returns List of top three populated cities of the inputted country
  */
  export const fetchThreeMostPopulatedCities = async ( countryInput:string ) => {
 
-    // TODO switch back from france to prop
-        return await fetchCountryCode(countryInput).then(
-            async (response) => {
-                return fetchThreeCities(response)
-            }, (error) => {
-                return console.error(error)
-            }
-        )
+    return await fetchCountryCode(countryInput).then(
+        async (response) => {
+            return fetchThreeCities(response)
+
+        }, (error) => {return console.error(error)}
+    )
 }
 
 
@@ -29,6 +27,7 @@
  * @returns The corresponding country code
  */
  const fetchCountryCode = async ( countryInput:string ) => {
+
     const endpoint:string = process.env.API_PATH + 'featureClass=A&featureCode=PCLI&maxRows=1&name=' + countryInput
 
     return fetch(endpoint).then(
@@ -37,12 +36,12 @@
                 if (result.totalResultsCount <= 0) {return "The country you have searched for does not exist"}
 
                 return result.geonames[0].countryCode
-            }, error => {return console.error(error)}
-        )
-        
-    )
- }
 
+            }, error => {return console.error(error)}
+        )   
+    )
+
+ }
 
 /**
  * 
@@ -50,20 +49,22 @@
  * @returns List of the three cities 
  */
  const fetchThreeCities = async ( countryCode:string ) => {
+
     const endpoint:string = process.env.API_PATH + 'featureCode=PPLA&featureCode=PPLS&featureCode=PPLC&maxRows=3&orderby=population&country=' + countryCode
 
     return fetch(endpoint).then(
         (response) => response.json().then(
             (result) => {
-                if (result.geonames.length <= 0) return "The country you have searched for does not exist"
+                if (result.geonames.length <= 0) {return "The country you have searched for does not exist"}
 
                 let cities:string[] = []
                 for (let i:number = 0; i < result.geonames.length; i++) {
                     cities.push(result.geonames[i].name)
                 }
-
                 return cities
+
             }, error => {return console.error(error)}
         )
     )
+
  }
--- api/cities.tsx
+++ api/cities.tsx
@@ -1,49 +1,21 @@
 /**
- * API for fetching information about cities 
+ * Methods for fetching information about cities 
  */
-
-/*
-const responseJSON = await response.json()
-        console.log(responseJSON)
-
-        if (responseJSON.totalResultsCount !== 1 ) {
-            return "The city you have searched for does not exist"
-        } else {
-            console.log(responseJSON.geonames[0].population)
-            let population = responseJSON.geonames[0].population
-            return population
-        }
-
-         try {
-        const response = await fetch(endpoint)
-        await response.json().then(
-            (result) => {
-                if (result.totalResultsCount !== 1) {return "The city you have searched for does not exist"}
-                
-                console.log(result.geonames[0].population)
-                let population = result.geonames[0].population
-                return population
-            }
-        )
-
-*/
-
-
 
 /**
  * Method that find population of a inputted city
  * 
  * @param cityInput The city the user has inputted 
- * @returns The population of the inputted city 
+ * @returns A number with the population of the inputted city 
  */
 export const fetchCityPopulation = async ( cityInput:string ) => {
 
+    // Uses environment variable set in the .env file
     const endpoint:string = process.env.API_PATH + 'featureCode=PPLA&featureCode=PPLS&featureCode=PPLC&maxRows=1&name_equals=' + cityInput
 
     return fetch(endpoint).then(
         (response) => response.json().then(
             (result) => {
-                console.log(result)
                 if (result.totalResultsCount !== 1) {return "The city you have searched for does not exist"}
                 
                 let population = result.geonames[0].population
@@ -51,7 +23,7 @@ export const fetchCityPopulation = async ( cityInput:string ) => {
                 
             }, (error) => { return error}        
         )
-    )
+    )  
         
 }
 
